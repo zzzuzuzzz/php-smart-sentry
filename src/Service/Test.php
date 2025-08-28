80aaa179a9df72a3a073e55fa3d0889ad9de929e
evgmezencev <evgmezencev@team.amocrm.com>
2025-08-28 21:15:46 +0300

    git init && create first test metods

---
+++ src/Service/Test.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Talib\PhpSmartSentry\Service;
+
+class Test
+{
+    public function returnTrue(): bool
+    {
+        return true;
+    }
+
+    public function returnFalse(): bool
+    {
+        return false;
+    }
+}
